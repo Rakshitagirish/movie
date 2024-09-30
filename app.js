0f4c73a393d38d5db8e8de3d7be090113b993644
Rakshita GF <107784752+Rakshitagirish@users.noreply.github.com>
2024-09-30 18:21:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const movieRoutes = require('./routes/movieRoutes');
 dotenv.config();
 const app = express();
 
-// Middleware
+//middleware
 app.use(bodyParser.json());
 
-// Connect to MongoDB
+// connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err));
 
-// Routes
+//api's
 app.use('/api', movieRoutes);
 
 // Start the server
